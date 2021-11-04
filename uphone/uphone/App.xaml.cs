55e1221ebdc831a2160a3cb46f451ac41672da1d
ehddnr177 <ehddnr177@uangel.com>
2021-11-05 08:09:38 +0900

    Initial commit by xamarin

---
+++ uphone/uphone/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace uphone
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new CallPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
